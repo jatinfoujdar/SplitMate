53cfba5561f28972ae6df51d11f7e5a25fb9d528
Jatin Foujdar <jatin321001@gmail.com>
2025-10-23 12:36:31 +0530

    Initial Commit

---
+++ SplitMate/SplitMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitMateApp.swift
+//  SplitMate
+//
+//  Created by jatin foujdar on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SplitMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
